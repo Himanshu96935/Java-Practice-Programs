2539a4054abc9e68c44ae5850f2d9d70e56cd08d
Himanshu Raj <himanshu44226688@gmail.com>
2025-07-25 23:24:32 +0530

    new program added

---
+++ For loop/src/NumberPrinting1.java
@@ -0,0 +1,12 @@
+public class NumberPrinting1
+{
+    public static void main (String args[])
+    {
+        int i;
+
+        for (i = 1; i <=10 ; i = i + 1)
+        {
+            System.out.println(i);
+        }
+    }
+}
