36a807787c2a271e9bc96e9ab8efc33f64df254b
Himanshu Raj <himanshu44226688@gmail.com>
2025-07-26 22:45:21 +0530

    new programs added in for loop module

---
+++ For loop/src/NumbersPrinting2.java
@@ -0,0 +1,11 @@
+public class NumbersPrinting2
+{
+    public static void main(String[] args) {
+
+        int i;
+        for(i=10 ; i <= 20 ; i = i + 1)
+        {
+            System.out.println(i);
+        }
+    }
+}
