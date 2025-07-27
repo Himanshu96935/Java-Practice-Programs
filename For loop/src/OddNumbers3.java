a0ee5775b55085ffab1da4c80afb431e1dffaffb
Himanshu Raj <himanshu44226688@gmail.com>
2025-07-27 22:18:36 +0530

    new code added

---
+++ For loop/src/OddNumbers3.java
@@ -0,0 +1,13 @@
+public class OddNumbers3
+{
+    public static void main (String args[])
+    {
+        int i;
+        for (i = 10; i <= 20; i = i +1)
+        {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
