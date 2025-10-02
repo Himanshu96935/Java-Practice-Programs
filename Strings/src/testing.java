00415fae753c9a71d3bef602749f3eac74b892c3
Himanshu Raj <himanshu44226688@gmail.com>
2025-10-02 17:43:35 +0530

    new programs added

---
+++ Strings/src/testing.java
@@ -0,0 +1,10 @@
+public class testing
+{
+    public static void main(String[] args) {
+        char ch = 'a';
+        for (int i = ch; i <= 'z'; i++)
+        {
+            System.out.print((char) i + " ");
+        }
+    }
+}
